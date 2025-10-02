a496ae4f595bfc69ebc98438cef534575ab9c8cf
Serge Kosachiov <skosachiov@gmail.com>
2025-10-02 11:19:54 +0300

    urljoin

--- distrotracker.py
+++ distrotracker.py
@@ -471,7 +471,7 @@ def main():
         help='set the logging level (default: %(default)s)')    
     
     args = parser.parse_args()
-    args.base_url = urljoin(args.base_url, '/')
+    if not args.base_url.endswith("/") args.base_url += "/"
 
     logging.basicConfig(level=getattr(logging, args.log_level), format='%(asctime)s %(levelname)s %(message)s')
 
